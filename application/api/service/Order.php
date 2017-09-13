3e311bfeba38b4205c64f1d0fba6271ddee14c4c
Hank Chen <onegledog@163.com>
2017-09-13 21:27:22 +0800

    修复下单接口json解码问题

--- application/api/service/Order.php
+++ application/api/service/Order.php
@@ -58,8 +58,7 @@ class Order
         $oProducts = OrderProduct::where('order_id','=',$orderID)->select();
         $order = OrderModel::where('id','=',$orderID)->find();
         $this->oProducts = $oProducts;
-        $expressInfo = json_decode($order->snap_express,true);
-        $this->oExpress = $expressInfo['express_price'];
+        $this->oExpress = $order->snap_express['express_price'];
         $this->products = $this->getProductsByOrder();
 
         $status = $this->getOrderStatus();
