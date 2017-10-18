109a6f63d52da7646ba53159e2533e377bb844e7
Hank Chen <onegledog@163.com>
2017-10-18 21:26:55 +0800

    修复抢购redis文件静态方法不应该使用非静态成员属性的问题

--- application/api/model/BuyNowRedis.php
+++ application/api/model/BuyNowRedis.php
@@ -5,8 +5,8 @@ namespace app\api\model;
 class BuyNowRedis extends BaseRedis
 {
 	const KEY_PREFIX = 'buynow_';
-	protected $dataPrefix = self::KEY_PREFIX . 'data:';
-	protected $stockPrefix = self::KEY_PREFIX . 'stock:';
+	protected static $dataPrefix = self::KEY_PREFIX . 'data:';
+	protected static $stockPrefix = self::KEY_PREFIX . 'stock:';
 	protected $batchID;
 
 	public function __construct($buyNowID)
@@ -18,21 +18,21 @@ class BuyNowRedis extends BaseRedis
 	{
 		$redis = self::getRedis();
 
-		return $redis->setNx($this->dataPrefix . $this->batchID, serialize($data));
+		return $redis->setNx(self::$dataPrefix . $this->batchID, serialize($data));
 	}
 
 	public function cacheStock($stock)
 	{
 		$redis = self::getRedis();
 
-		return $redis->setNx($this->stockPrefix . $this->batchID, $stock);
+		return $redis->setNx(self::$stockPrefix . $this->batchID, $stock);
 	}
 
 	public static function getData($buyNowID, $syncStock = true)
 	{
 		$redis = self::getRedis();
 
-		$buyNow = $redis->get($this->dataPrefix . $buyNowID);
+		$buyNow = $redis->get(self::$dataPrefix . $buyNowID);
 
 		if ($syncStock) {
 			$nowStock = self::getStock($buyNowID);
@@ -46,7 +46,7 @@ class BuyNowRedis extends BaseRedis
 	{
 		$redis = self::getRedis();
 
-		return $redis->get($this->stockPrefix . $buyNowID);
+		return $redis->get(self::$stockPrefix . $buyNowID);
 	}
 
 	public static function batchDecrStock($orderProduct)
@@ -54,8 +54,8 @@ class BuyNowRedis extends BaseRedis
 		$redis = self::getRedis();
 		
 		foreach ($orderProduct as $singleProduct) {
-			if ($redis->decr($this->stockPrefix . $singleProduct['batch_id'], $singleProduct['counts']) < 0) {
-				$redis->incr($this->stockPrefix . $singleProduct['batch_id'], $singleProduct['counts']);
+			if ($redis->decr(self::$stockPrefix . $singleProduct['batch_id'], $singleProduct['counts']) < 0) {
+				$redis->incr(self::$stockPrefix . $singleProduct['batch_id'], $singleProduct['counts']);
 				return false;
 			}
 		}
