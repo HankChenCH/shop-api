d50e70ff344e1c4b0930e470e0e010df9b5018bc
Hank Chen <onegledog@163.com>
2017-10-26 20:00:10 +0800

    增加主题排序验证器

---
+++ application/api/validate/ThemeRank.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace app\api\validate;
+
+use app\lib\exception\ThemeException;
+
+class ThemeRank extends BaseValidate
+{
+	protected $rule = [
+		'ranks' => 'require|isNotArrayEmpty|checkRanks'
+	];
+
+	protected $singleRule = [
+		'id' => 'require|isPostiveInteger',
+		'rank' => 'require|number'
+	]
+
+	protected function checkRanks($values)
+	{
+		foreach ($values as $value) {
+    		$this->checkRank($value);
+    	}
+
+        return true;
+	}
+
+	private function checkRank($value)
+    {
+        $validate = new BaseValidate($this->singleRule);
+        $result = $validate->batch()->check($value);
+        if (!$result) {
+            throw new ThemeException([
+                'msg' => '主题排序参数错误'
+            ]);
+        }
+    }
+}
